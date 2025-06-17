fbc4c8ee7d8a36e0ab162332761935b0696bf249
Houbovo <44392566+Houbovo@users.noreply.github.com>
2025-06-17 10:12:29 +0200

    Update lekce8_hw_prep.sh

--- Lekce8/HW/lekce8_hw_prep.sh
+++ Lekce8/HW/lekce8_hw_prep.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/bash
 
 sudo systemctl disable chronyd.service --now > /dev/null 2>&1
+sudo systemctl disable vboxadd-service.service --now > /dev/null 2>&1
 sudo systemctl mask chronyd.service > /dev/null 2>&1
 
 sudo sed -i "s/^pool/gibberish/g" /etc/chrony.conf
 
 sudo timedatectl set-time "2012-10-30 18:17:16" > /dev/null
-
+sleep 5
 sudo hwclock -s > /dev/null
